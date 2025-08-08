dd2932ff02be1ce602de7ae62ceb732088a85997
Nicolas Tröndle <94861272+Ntrondle@users.noreply.github.com>
2025-08-08 23:21:04 +0200

    favico

--- app/layout.js
+++ app/layout.js
@@ -12,6 +12,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      </head>
       <body>
         <header className="border-b border-neutral-200 dark:border-neutral-800">
           <div className="container flex items-center justify-between py-3">
