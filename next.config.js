3f5d971ae8a56139a7f966cb63124bf323f0167f
Nicolas Tröndle <94861272+Ntrondle@users.noreply.github.com>
2025-08-08 11:46:25 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+const repo = 'tes-site'; // your repo name here
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
+  trailingSlash: true,
+  basePath: isProd ? `/${repo}` : '',
+  assetPrefix: isProd ? `/${repo}/` : '',
 };
+
 module.exports = nextConfig;
