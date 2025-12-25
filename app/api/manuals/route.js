e7c25ff202e655d512b842ea48d5fdf5c6981c31
Nicolas Tröndle <94861272+Ntrondle@users.noreply.github.com>
2025-12-25 11:08:42 +0100

    Update route.js

--- app/api/manuals/route.js
+++ app/api/manuals/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server'
-import { getManuals } from '../../../../lib/manuals'
+import { getManuals } from '../../../lib/manuals'
 
 export async function GET(request) {
   const { searchParams } = new URL(request.url)
